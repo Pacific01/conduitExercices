f85d10cbc42f761eb39486cc88a8c9735bfb2d25
Jaime Pascual Torres <jpascual@everlyrusher.com>
2022-12-24 17:45:12 +0100

    First Exercise

--- app/Main.hs
+++ app/Main.hs
@@ -6,3 +6,8 @@ main :: IO ()
 main = do
   -- Pure operations: summing numbers.
   print $ runConduitPure $ yieldMany [1..10] .| sumC
+
+-- Exercise Work through what happens when we add .| mapM_C print to the mix
+-- above.
+-- Thought: mapM_C print ::           ConduitT String IO ()  IO ()
+-- Reality: mapM_C print :: Show a => ConduitT a      o       IO ()
